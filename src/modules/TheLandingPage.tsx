465320d94e87a74f7ff4bb10d05d605c63f2c5d4
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2024-10-22 17:20:42 +0700

    feat: add carousel to the landing page

--- src/modules/TheLandingPage.tsx
+++ src/modules/TheLandingPage.tsx
@@ -1,8 +1,23 @@
-import * as React from 'react'
+import { Carousel } from "antd";
 
+const DEFINE_IMG_CAROUSEL = [
+  "/landing_page/landing_page_1.jpg",
+  "/landing_page/landing_page_2.jpg",
+  "/landing_page/landing_page_3.jpg",
+  "/landing_page/landing_page_4.jpg",
+];
 
 export default function TheLandingPage() {
   return (
-    <div>TheLandingPage</div>
-  )
+    <div className="flex flex-col w-full justify-start items-center">
+      <div className="w-full">
+        <Carousel autoplay>
+          {DEFINE_IMG_CAROUSEL.map((item) => (
+            <img className="w-full h-[620px] object-cover" src={item} />
+          ))}
+        </Carousel>
+      </div>
+      <h2>TheLandingPage</h2>
+    </div>
+  );
 }
